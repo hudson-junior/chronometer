74600423f53eab74f2a3c4c1ce2f9daeec1943f2
hudson-junior <hjunior2702@gmail.com>
2025-05-30 14:28:03 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,89 @@
+const btnStart = document.getElementById("start");
+const btnPause = document.getElementById("pause");
+const btnReset = document.getElementById("reset");
+const btnLap = document.getElementById("lap");
+const lapsList = document.getElementById("laps");
+const display = document.getElementById("display");
+const centDisplay = document.getElementById("cent-display");
+const secDisplay = document.getElementById("sec-display");
+const minDisplay = document.getElementById("min-display");
+
+let hundreds = 0;
+let seconds = 0;
+let minutes = 0;
+let centCount = null;
+let numberLaps = 0;
+
+const playTimer = () => {
+  centCount = setInterval(() => {
+    hundreds++;
+    if (hundreds >= 100) {
+      seconds++;
+      hundreds = 0;
+    }
+    if (seconds >= 60) {
+      minutes++;
+      seconds = 0;
+    }
+    centDisplay.textContent = `.${format(hundreds)}`
+    secDisplay.textContent = `${format(seconds)}`
+    minDisplay.textContent = `${format(minutes)}`
+  },
+    10);
+  display.style.color = "#29CD62";
+  centDisplay.style.color = "#29CD62";
+  btnStart.style.display = "none"
+  btnReset.style.display = "none"
+  btnPause.style.display = "inline"
+  btnLap.style.display = "inline"
+  return
+}
+
+const pauseTimer = () => {
+  clearInterval(centCount);
+
+  btnStart.textContent = "Continuar"
+  btnStart.style.display = "inline"
+  btnReset.style.display = "inline"
+  btnPause.style.display = "none"
+  btnLap.style.display = "none"
+  display.style.color = "#CD2929";
+  centDisplay.style.color = "#CD2929";
+  return centCount
+}
+
+const format = (num) => {
+  if (num < 10) {
+    num = "0" + num;
+  }
+  return num
+}
+
+const resetTimer = () => {
+  clearInterval(centCount);
+  centCount = null;
+
+  hundreds = 0;
+  seconds = 0;
+  minutes = 0;
+  numberLaps = 0;
+  centDisplay.textContent = `.${format(hundreds)}`
+  secDisplay.textContent = `${format(seconds)}`
+  minDisplay.textContent = `${format(minutes)}`
+
+  btnStart.textContent = "Iniciar"
+  btnStart.style.display = "inline"
+  btnReset.style.display = "inline"
+  btnPause.style.display = "none"
+  btnLap.style.display = "none"
+  display.style.color = "#ffffff";
+  centDisplay.style.color = "#ffffff";
+  lapsList.innerHTML = "";
+}
+
+const lapTimer = () => {
+  const li = document.createElement("li");
+  li.textContent = `Volta ${++numberLaps}: ${format(minutes)}:${format(seconds)}.${format(hundreds)}`;
+  li.style.color = '#CDB729';
+  lapsList.appendChild(li);
+}
